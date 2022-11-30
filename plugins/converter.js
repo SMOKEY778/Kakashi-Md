193ee0ddc0ddb561026e561644bc6fdf0a000d7b
Toxic-Turbo <97358745+TOXICTURBO@users.noreply.github.com>
2022-11-30 15:57:48 +0530

    Update converter.js

--- plugins/converter.js
+++ plugins/converter.js
@@ -1,21 +1,16 @@
 const {
-    command, isPublic
+    command, isPrivate
 } = require('../lib/');
 const fs = require('fs');
 const ffmpeg = require('fluent-ffmpeg');
 const Config = require('../config');
-const {
-    getString
-} = require('../lib/misc/lang');
-const Lang = getString('converters');
-
 command({
     pattern: 'mp3 ?(.*)',
-    fromMe: isPublic,
+    fromMe: isPrivate,
     type: 'edit',
-    desc: Lang.MP3_DESC
+    desc: 'Convert Video/Vn To Audio'
 }, (async (message, match) => {
-    if (message.reply_message === false) return await message.reply(Lang.MP3_NEED_REPLY)
+    if (message.reply_message === false) return await message.reply(`Reply To Video Or Voice Note`)
     var {seconds} = message.quoted.message[Object.keys(message.quoted.message)[0]];
     if (seconds>120) await message.reply(`_Alert: Duration more than 2 mins. This process may fail or take much more time!_`)
     var savedFile = await message.reply_message.download();
@@ -23,7 +18,7 @@ command({
         .save('./temp/tomp3.mp3')
         .on('end', async () => {
             await message.client.sendMessage(message.jid, {
-                audio: fs.readFileSync('./temp/tomp3.mp3'),
+                audio: fs.readFileSync('./tomp3.mp3'),
                 mimetype: 'audio/mp4',
                 ptt: false
             }, {
@@ -34,11 +29,11 @@ command({
 
 command({
     pattern: 'photo ?(.*)',
-    fromMe: isPublic,
+    fromMe: isPrivate,
     type: 'edit',
-    desc: Lang.PHOTO_DESC
+    desc: 'Convert Sticker To Photo'
 }, (async (message, match) => {
-    if (message.reply_message === false) return await message.send(Lang.PHOTO_NEED_REPLY)
+    if (message.reply_message === false) return await message.send(`Reply To Sticker`)
         var savedFile = await message.reply_message.download();
         ffmpeg(savedFile)
             .fromFormat('webp_pipe')
