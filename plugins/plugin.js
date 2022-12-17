143481f753de6092a53b386cb969c3b52e9375fb
Toxic-Turbo <97358745+TOXICTURBO@users.noreply.github.com>
2022-12-17 09:28:39 +0530

    Update plugin.js

--- plugins/plugin.js
+++ plugins/plugin.js
@@ -3,53 +3,58 @@ const got = require("got");
 const fs = require("fs");
 const { PluginDB, installPlugin } = require("../lib/database/plugins");
 
+
+
 command(
   {
-    pattern: "install ?(.*)",
+    pattern: "install",
     fromMe: true,
     desc: "Installs External plugins",
-    type:'user'
+    type: "user",
   },
   async (message, match) => {
     if (!match) return await message.sendMessage("_Send a plugin url_");
-    for (let Url of getUrl(match)) {
-      try {
-        var url = new URL(Url);
-      } catch {
-        return await message.sendMessage("_Invalid Url_");
-      }
 
-      if (url.host === 'gist.github.com') {
-        url.host = 'gist.githubusercontent.com';
-        url = url.toString() + '/raw'
-    } else {
-        url = url.toString()
+    try {
+      var url = new URL(match);
+    } catch (e) {
+      console.log(e);
+      return await message.sendMessage("_Invalid Url_");
     }
 
-    var response = await got(url);
-    if (response.statusCode == 200) {
-        let plugin_name = /pattern: ["'](.*)["'],/g.exec(response.body)
-	plugin_name = plugin_name[1].split(' ')[0]
-        fs.writeFileSync("./plugins/" + plugin_name + ".js", response.body);
-        try {
-          require("./" + plugin_name);
-        } catch (e) {
-          fs.unlinkSync(__dirname+"/" + plugin_name + ".js");
-          return await message.sendMessage("Invalid Plugin\n ```" + e + "```");
-        }
-
-        await installPlugin(url, plugin_name);
-
-        await message.sendMessage(
-          `_New plugin installed : idk too🥲`
-        );
+    if (url.host === "gist.github.com") {
+      url.host = "gist.githubusercontent.com";
+      url = url.toString() + "/raw";
+    } else {
+      url = url.toString();
+    }
+    var plugin_name;
+    var { body, statusCode } = await got(url);
+    if (statusCode == 200) {
+      var comand = body.match(/(?<=pattern:) ["'](.*?)["']/);
+      plugin_name = comand[0].replace(/["']/g, "").trim().split(" ")[0];
+      if (!plugin_name) {
+        plugin_name = "__" + Math.random().toString(36).substring(8);
       }
+      fs.writeFileSync(__dirname + "/" + plugin_name + ".js", body);
+      try {
+        require("./" + plugin_name);
+      } catch (e) {
+        fs.unlinkSync(__dirname + "/" + plugin_name + ".js");
+        return await message.sendMessage("Invalid Plugin\n ```" + e + "```");
+      }
+
+      await installPlugin(url, plugin_name);
+
+      await message.sendMessage(`_New plugin installed : ${plugin_name}_`);
     }
   }
 );
 
+
+
 command(
-  { pattern: "plugin", fromMe: true, desc: "plugin list" ,type:'user'},
+  { pattern: "plugin", fromMe: true, desc: "plugin list", type: "user" },
   async (message, match) => {
     var mesaj = "";
     var plugins = await PluginDB.findAll();
@@ -69,12 +74,14 @@ command(
   }
 );
 
+
+
 command(
   {
     pattern: "remove(?: |$)(.*)",
     fromMe: true,
     desc: "Remove external plugins",
-    type:'user'
+    type: "user",
   },
   async (message, match) => {
     if (!match) return await message.sendMessage("_Need a plugin name_");
@@ -86,7 +93,7 @@ command(
     } else {
       await plugin[0].destroy();
       delete require.cache[require.resolve("./" + match + ".js")];
-      fs.unlinkSync("./plugins/" + match + ".js");
+      fs.unlinkSync(__dirname + "/" + match + ".js");
       await message.sendMessage(`Plugin ${match} deleted`);
     }
   }
